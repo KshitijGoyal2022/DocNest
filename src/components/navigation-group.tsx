e8d7dec96a4b822c7fd8ea4225374a7e8e818b07
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2024-05-01 12:45:01 -0700

    active page marker

--- src/components/navigation-group.tsx
+++ src/components/navigation-group.tsx
@@ -26,15 +26,6 @@ export default function NavigationGroup({
       >
         <div className="text-xl font-semibold flex justify-between items-center bg-gray-100 hover:bg-gray-300 text-gray-700 hover:text-gray-900 rounded-lg" onClick={toggleChildren}>
           <NavLink id={node.id}>{node.title}</NavLink>
-          {/* {node.children.length > 0 && (
-            <button
-              onClick={toggleChildren}
-              className="text-sm px-2 py-1 rounded-md"
-              aria-label={isOpen ? "Collapse" : "Expand"}
-            >
-              {isOpen ? <FaArrowUp className='w-3 h-3'/> : <FaArrowDown className='w-3 h-3' />}
-            </button>
-          )} */}
         </div>
         {isOpen && (
           <ul role="list">
--- src/components/navlink.tsx
+++ src/components/navlink.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 
 export default function NavLink({
   id,
@@ -9,10 +11,14 @@ export default function NavLink({
   children: React.ReactNode;
   icon?: React.ReactNode; 
 }) {
+
+  const pathname = usePathname();
+  const isActive = pathname === `/docs/${id}`; // Update this path as necessary to match your routing structure
+  const activeClass = "border-l-4 border-blue-500";
   return (
     <Link
       href={`/docs/${id}`}
-      className='flex items-center justify-between gap-2 py-2 pr-3 pl-4 text-sm transition-colors duration-300 ease-in-out rounded-md '
+      className={`flex items-center justify-between gap-2 py-2 pr-3 pl-4 text-sm transition-colors duration-300 ease-in-out rounded-md ${isActive ? activeClass : ''}`}
     >
       {icon && <span className='mr-2 flex items-center'>{icon}</span>}
       <span className='truncate flex-1'>{children}</span>
