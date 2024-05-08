921b5e48a0b58eb9cf0efd739b6fc2e7312f235a
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2024-05-08 16:40:06 -0700

    improved ui

--- src/components/right-sidebar.tsx
+++ src/components/right-sidebar.tsx
@@ -57,12 +57,12 @@ export default function RightSidebar({ recordMap }) {
     }, [selectedId]); // Include selectedId in the dependency array to update component with state changes
 
     return (
-      <div className='w-64 bg-white overflow-y-auto fixed right-20 top-30 p-5 border-gray-400 border-b'>
+      <div className='w-64 bg-white overflow-y-auto fixed right-20 top-30 p-5 border-gray-200 border-b'>
         <span className='text-black text-sm font-medium'>On this page</span>
         <ul className='list-none p-0 mt-3'>
           {headings.map((heading, index) => (
             <li key={index} className='mb-1'>
-              <a href={`#${heading.id}`} className={` hover:text-gray-600 text-sm font-medium ${selectedId === heading.id ? 'text-sky-600' : 'text-gray-400'}`}>
+              <a href={`#${heading.id}`} className={` hover:text-gray-500 text-sm font-medium ${selectedId === heading.id ? 'text-sky-600' : 'text-gray-400'}`}>
                 {heading.text}
               </a>
             </li>
