a4a18307f894fc7cc9e720dc7fe3282cafee50f2
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2024-05-09 10:26:50 -0700

    right sidebar scrolling

--- src/components/right-sidebar.tsx
+++ src/components/right-sidebar.tsx
@@ -1,73 +1,103 @@
-'use client'
+'use client';
 import { useEffect, useState } from 'react';
 
 export default function RightSidebar({ recordMap }) {
-    const [selectedId, setSelectedId] = useState(null); // State to track selected ID
+  const [selectedId, setSelectedId] = useState(null); // State to track selected ID
 
-    const extractHeadings = (recordMap) => {
-      const headings = [];
-  
-      if (recordMap && recordMap.block) {
-        for (const blockId in recordMap.block) {
-          const block = recordMap.block[blockId].value;
-          if (['header', 'sub_header', 'sub_sub_header'].includes(block.type)) {
-            const dataId = blockId.replace(/-/g, '');
-            headings.push({
-              id: dataId,
-              text: block.properties?.title[0][0],
-            });
-          }
+  const extractHeadings = (recordMap) => {
+    const headings = [];
+
+    if (recordMap && recordMap.block) {
+      for (const blockId in recordMap.block) {
+        const block = recordMap.block[blockId].value;
+        if (['header', 'sub_header', 'sub_sub_header'].includes(block.type)) {
+          const dataId = blockId.replace(/-/g, '');
+          headings.push({
+            id: dataId,
+            text: block.properties?.title[0][0],
+          });
         }
       }
-      return headings;
+    }
+    return headings;
+  };
+
+  const headings = extractHeadings(recordMap);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = 130; // You can adjust this threshold
+      const scrollPosition = window.scrollY + offset;
+      let closestHeading = null;
+      let smallestDiff = Infinity; // This will track the smallest difference
+      let reachedBottom =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight;
+
+      headings.forEach((heading) => {
+        const el = document.getElementById(heading.id);
+        if (el) {
+          const elementPosition =
+            el.getBoundingClientRect().top + window.scrollY;
+          const diff = Math.abs(elementPosition - scrollPosition); // Difference from the scroll position
+
+          if (elementPosition >= scrollPosition && diff < smallestDiff) {
+            smallestDiff = diff;
+            closestHeading = heading.id;
+          }
+        }
+      });
+      if (reachedBottom) {
+        closestHeading = headings[headings.length - 1]?.id;
+      }
+
+      setSelectedId(closestHeading);
     };
 
-    const headings = extractHeadings(recordMap);
-    console.log(headings)
+    const handleLinkClick = (event) => {
+      event.preventDefault();
+      const targetId = event.target.getAttribute('href').substring(1);
+      setSelectedId(targetId);
+      const targetElement = document.getElementById(targetId);
 
-    useEffect(() => {
-        const handleLinkClick = event => {
-            event.preventDefault();
-            const targetId = event.target.getAttribute('href').substring(1);
-            console.log("Target ID:", targetId);
-            setSelectedId(targetId);  // Update the selectedId state on click
-            const targetElement = document.querySelector(`[data-id="${targetId}"]`);
-
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                });
-            }
-        };
-
-        const anchors = document.querySelectorAll('a[href^="#"]');
-        anchors.forEach(anchor => {
-            anchor.addEventListener('click', handleLinkClick);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
         });
+      }
+    };
 
-        // Debug: Log the current selectedId
-        console.log("Current selectedId:", selectedId);
-
-        return () => {
-            anchors.forEach(anchor => {
-                anchor.removeEventListener('click', handleLinkClick);
-            });
-        };
-    }, [selectedId]); // Include selectedId in the dependency array to update component with state changes
-
-    return (
-      <div className='w-64 bg-white overflow-y-auto fixed right-20 top-30 p-5 border-gray-200 border-b'>
-        <span className='text-black text-sm font-medium'>On this page</span>
-        <ul className='list-none p-0 mt-3'>
-          {headings.map((heading, index) => (
-            <li key={index} className='mb-1'>
-              <a href={`#${heading.id}`} className={` hover:text-gray-500 text-sm font-medium ${selectedId === heading.id ? 'text-sky-600' : 'text-gray-400'}`}>
-                {heading.text}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
+    window.addEventListener('scroll', handleScroll);
+    const anchors = document.querySelectorAll('a[href^="#"]');
+    anchors.forEach((anchor) =>
+      anchor.addEventListener('click', handleLinkClick)
     );
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      anchors.forEach((anchor) =>
+        anchor.removeEventListener('click', handleLinkClick)
+      );
+    };
+  }, [headings]); // Include selectedId in the dependency array to update component with state changes
+
+  return (
+    <div className='w-64 bg-white overflow-y-auto fixed right-20 top-30 p-5 border-gray-200 border-b hidden lg:block'>
+      <span className='text-black text-sm font-medium'>On this page</span>
+      <ul className='list-none p-0 mt-3'>
+        {headings.map((heading, index) => (
+          <li key={index} className='mb-1'>
+            <a
+              href={`#${heading.id}`}
+              className={` hover:text-gray-500 text-sm font-medium ${
+                selectedId === heading.id ? 'text-sky-600' : 'text-gray-400'
+              }`}
+            >
+              {heading.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
