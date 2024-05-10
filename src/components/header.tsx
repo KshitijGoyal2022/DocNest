c1c6d2e2c678a67779db92ca69cac7bbeffc0e2e
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2024-05-10 12:45:47 -0700

    search bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,20 @@
-'use client'
+'use client';
 import { signOut } from 'next-auth/react';
+import { useState } from 'react';
 
 export default function Header() {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
     <div className='border-b border-gray-200 py-5 flex justify-between'>
-      <h3 className='flex text-base font-semibold leading-6 text-gray-900 pl-10 items-center '>
-        Verosoft Design Documentation Center
-      </h3>
+      <div className='flex items-center pl-10 '>
+        <input
+          type='text'
+          placeholder='Search...'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className='text-base leading-6 w-64 md:w-96  border border-gray-300 focus:border-sky-500 focus:ring focus:ring-sky-500 focus:ring-opacity-50 rounded-lg shadow-sm pl-4 pr-3 py-2'
+        />
+      </div>
       <div
         className='mr-4 text-white bg-sky-600 hover:bg-sky-700 cursor-pointer p-2 rounded-lg'
         onClick={() => signOut()}
